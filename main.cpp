a54d3db47c354ed9cb876212285eda3d4cd08c4e
steven-nguyen22 <snguy117@ucr.edu>
2022-05-10 14:48:27 -0700

    start

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    //menu for the program
+    cout << "Welcome to Steven Nguyen's Feature Selection Algorithm.\n\n";
+    cout << "Please enter the total number of features: ";
+    int featureChoice;
+    cin >> featureChoice;
+    cin.ignore();
+
+
+    //choosing which type of search algorithm to apply to the puzzle
+    cout << "\nEnter your choice of algorithm\n";
+    cout << "1. Forward Selection\n";
+    cout << "2. Backward Elimination\n";
+    cout << "3. My algorithm\n";
+    int algorithm; 
+    cin >> algorithm;
+    if(algorithm == 1) {
+      
+    }
+    if(algorithm == 2) {
+       
+    }
+    if(algorithm == 3) {
+        
+    }
+
+
+    return 0;
+}
